bd9245841acd66e23c4e03306c6e5834b8825941
qwertyuiii-code <85434511+qwertyuiii-code@users.noreply.github.com>
2022-01-07 17:56:05 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_certus.mk
+    $(LOCAL_DIR)/cherish_certus.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_certus-user \
-    nad_certus-userdebug \
-    nad_certus-eng
+    cherish_certus-user \
+    cherish_certus-userdebug \
+    cherish_certus-eng
